df5e057d18c749b506221a52d78dbc11315b15af
KaritaLaine <44746@edu.raseko.fi>
2024-03-14 11:24:43 +0200

    kirjautumis popup

--- opiskeluseura/verkkosivut/static/popup.js
+++ opiskeluseura/verkkosivut/static/popup.js
@@ -1,14 +1,16 @@
 function openForm() {
+    // Kun kirjautumisnappia painetaan, kirjautumis-form tulee esille
     document.getElementById('myForm').style.display = 'block';
-    // Create a backdrop element and append it to the body
+    // Lisätään backdrop-elementti sivulle
     var backdrop = document.createElement('div');
     backdrop.className = 'backdrop';
     document.body.appendChild(backdrop);
 }
 
 function closeForm() {
+    // Kun sulkunappia painetaan, kirjautumis-form poistuu näkyvistä
     document.getElementById("myForm").style.display = "none";
-    // Remove the backdrop when closing the form
+    // Kun kirjautumis-form suljetaan, poistetaan backdrop-elementti
     var backdrop = document.querySelector('.backdrop');
     if (backdrop) {
         backdrop.parentNode.removeChild(backdrop);
